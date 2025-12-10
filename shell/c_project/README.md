53aefbc4617440bda4bff233392f061a66b9dd82
CYA <3155414334@qq.com>
2025-12-10 19:31:10 +0800

    c_project:完善了模板文件功能和新增了创建模块功能
    完善了模板文件功能,现在能够更灵活的创建空项目了
    创建空文件时显示被创建文件的逻辑修改了,现在更自动化了
    完善了C项目目录结构(具体参考README.md)
    用新的格式翻新了老的模板文件,并更具目录结构新增了几个模板文件
    新增了创建模块功能,可以针对某一个项目自动的创建一个空模块

--- shell/c_project/README.md
+++ shell/c_project/README.md
@@ -2,4 +2,49 @@
 用于快速创建一个空的C项目文件架构
 
 # 具体用法
-参考脚本的文本或者执行脚本的 `-h` 选项
+- 参考脚本的文本或者执行脚本的 `-h` 选项
+- text_template/ 目录下的模板文件可用于规定额外生成文件
+  脚本在用户使用 `-c` 选项时会默认根据格式创建对应的文件
+  格式为: `<相对于项目根目录的路径>_<文件名>_template.txt`
+  说起来有点奇怪，如果你不想模板文件被自动创建
+  换句话说你对某个模板文件有特殊的用处，你可以用以下的格式命名
+  格式为: `.<注释>_<文件名>_template.txt`
+  此外, 这些模板文件中可以使用特定的宏
+  格式为: `** macro **`
+  目前仅支持以下的宏:
+  - `** project_name_replace **` - 表示项目的名称 
+  - `** exe_name_replace **`     - 表示可执行文件的名称(默认同项目的名称)
+  值得注意的是下述的几个模板文件可以(适当地)修改，但是不建议删除
+  - `text_template/.module_CMakeLists.txt_template.txt`
+  - `text_template/root_.gitignore_template`
+  - `text_template/root_CMakeLists.txt_template`
+  - `text_template/src_CMakeLists.txt_template`
+  - `text_template/src_main.c_template`
+
+# 参考的C项目目录结构
+``` 项目目录
+project_name/                   # 项目根目录
+├── src/                        # 源代码目录（核心逻辑）
+│   ├── main.c                  # 程序入口（main函数）
+│   ├── module1/                # 模块1（如网络模块）
+│   │   ├── include/
+│   │   │   └── module1_priv.h  # 模块1私有头文件（仅能被模块内使用）
+│   │   ├── module1.c           # 模块1实现
+│   │   └── CMakeLists.txt
+│   ├── module2/                # 模块2（如工具函数）
+│   │   ├── include/
+│   │   │   └── module2_priv.h  # 模块2私有头文件（仅能被模块内使用）
+│   │   ├── module2.c           # 模块2实现
+│   │   └── CMakeLists.txt
+│   └── CMakeLists.txt          # 组织src下的所有模块, 进行构建(可执行程序或者库)
+├── include/                    # 公共头文件目录（供外部引用）
+│   ├── module1.h               # 模块1的公共头文件 （对外接口）
+|   └── module2.h           
+├── lib/                        # 第三方库或静态/动态库
+│   ├── libxxx.a                # 静态库
+│   └── libxxx.so               # 动态库（Linux）/ libxxx.dll（Windows）
+├── build/                      # 编译生成的各种文件
+├── bin/                        # 最终可执行文件
+├── CMakeLists.txt              # 定义项目、添加子目录、全局设置
+└── README.md                   # 项目说明（功能、编译方式、使用方法）
+```
--- shell/c_project/c_project.sh
+++ shell/c_project/c_project.sh
@@ -9,10 +9,15 @@ if [ ! $(basename $0) = "c_project.sh" ]; then
 else
     CONFIG_DIR="$(dirname $0)/"
 fi
-MACRO_HEAD="\*\*"
-MACRO_RAIL="\*\*"
-PROJECT_NAME_MACRO="$MACRO_HEAD project_name_replace $MACRO_RAIL"
-EXE_NAME_MACRO="$MACRO_HEAD exe_name_replace $MACRO_RAIL"
+MACRO_NORMAL_HEAD="\*\*"
+MACRO_NORMAL_RAIL="\*\*"
+MACRO_UPPER_HEAD="\*U"
+MACRO_UPPER_RAIL="U\*"
+MACRO_LOWER_HEAD="\*L"
+MACRO_LOWER_RAIL="L\*"
+PROJECT_NAME_MACRO="project_name_replace"
+EXE_NAME_MACRO="exe_name_replace"
+MODULE_NAME_MACRO="module_name_replace"
 
 # 选项判断
 IS_CREATE_PROJECT=false
@@ -20,24 +25,31 @@ IS_CREATE_ADDITIONAL_GIT=false
 IS_BUILD_PROJECT=false
 IS_RUN_EXECUTE=false
 IS_NO_OUTPUT=false
+IS_CREATE_MODULE=false
 
 # 帮助信息
 function help {
-    echo "用法: $TOOL_NAME [选项] <参数>"
+    echo "用法: $TOOL_NAME [选项] <参数...>"
     echo
     echo "选项:"
-    echo "  -b <目录>    构建指定目录下的C项目(目录下必须包含CMakeLists.txt)"
-    echo "  -c <目录>    创建一个新的C项目, 位置为指定目录(项目默认与末级目录同名)"
-    # echo "  -e <名字>    创建新的C项目时指定可执行文件的名字 (暂未实现)"
-    echo "  -g          创建新的C项目时, 额外初始化git"
-    echo "  -h          显示此帮助信息(运行此选项会忽略其他所有选项)"
-    echo "  -n          不输出任何 -b -c -r 选项的信息(错误任会显示)"
-    # echo "  -p          创建新的C项目时指定项目的名字 (暂未实现)"
-    echo "  -r <目录>    运行指定目录下C项目的可执行文件(目录/bin/项目名)"
+    echo "  -b <目录...>    构建指定目录下的C项目(目录下必须包含CMakeLists.txt)"
+    echo "  -c <目录...>    创建新的C项目, 位置为指定目录(项目默认与末级目录同名)"
+    # echo "  -e <名字>     创建新的C项目时指定可执行文件的名字 (暂未实现)"
+    echo "  -g              创建新的C项目时, 额外初始化git"
+    echo "  -h              显示此帮助信息(运行此选项会忽略其他所有选项)"
+    echo "  -m <名字>       创建一个空的项目模块(存放于项目的src目录下) (暂未实现)"
+    echo "  -n              不输出任何 -b -c -r 选项的信息(错误任会显示)"
+    # echo "  -p            创建新的C项目时指定项目的名字 (暂未实现)"
+    echo "  -r <目录...>     运行指定目录下C项目的可执行文件(目录/bin/项目名)"
     echo "示例:"
-    echo "  $TOOL_NAME -c ~/my_c_project"
-    echo "  $TOOL_NAME -br ~/my_c_project"
-    echo "  $TOOL_NAME -gcbr ~/my_c_project"
+    echo "  $TOOL_NAME -c ./my_c_project"
+    echo "  $TOOL_NAME -br ./my_c_project"
+    echo "  $TOOL_NAME -gcbr ./my_c_project"
+    echo "  $TOOL_NAME -m ./my_c_project my_c_module_name"
+    echo "注意:"
+    echo "  创建项目、编译项目、执行项目可执行文件可以连续进行,"
+    echo "  但是它们均与创建模块选项冲突，冲突时默认只创建模块，忽略 -b -c -r 选项。"
+    echo "  创建模块选项一次性只能针对一个项目创建一个模块。 "
     echo
     return 0
 }
@@ -53,16 +65,6 @@ function make_path_pretty {
     local pretty_path=$(echo "$raw_path" | sed 's|///*|/|g')
     echo "$pretty_path"
 }
-# 解析宏定义
-function deal_macro {
-    local file_path="$1"
-    local macro_key="$2"
-    local macro_value="$3"
-
-    sed -i "{   s/$macro_key/$macro_value/g
-            }" "$file_path"
-}
-
 # 获取项目名称
 function get_project_name {
     local project_dir="$1/"
@@ -73,7 +75,19 @@ function get_project_name {
     fi
     echo "$project_name"
 }
+# 解析宏定义
+function deal_macro {
+    local file_path="$1"
+    local macro_key="$2"
+    local macro_value="$3"
+    local macro_value_upper="$(echo "$macro_value" | tr '[:lower:]' '[:upper:]')"
+    local macro_value_lower="$(echo "$macro_value" | tr '[:upper:]' '[:lower:]')"
 
+    sed -i "{   s/$MACRO_NORMAL_HEAD $macro_key $MACRO_NORMAL_RAIL/$macro_value/g
+                s/$MACRO_UPPER_HEAD $macro_key $MACRO_UPPER_RAIL/$macro_value_upper/g
+                s/$MACRO_LOWER_HEAD $macro_key $MACRO_LOWER_RAIL/$macro_value_lower/g
+            }" "$file_path"
+}
 # 删除冲突文件或目录, 由于涉及 rm 命令，故小心使用，避免删除重要文件
 function remove_conflict {
     local target_path="$1"
@@ -88,7 +102,108 @@ function remove_conflict {
             return 1;;
     esac
 }
+# 模拟tree命令
+function my_tree {
+    local indent="${2:-}"
+    local item
+    
+    for item in "$1"/*; do
+        [ -e "$item" ] || continue  # 跳过不存在的文件
+        
+        # 获取文件名（不含路径）
+        local name=$(basename "$item")
+        
+        # 打印当前项（目录或文件）
+        echo "${indent}├── $name"
+        
+        # 如果是目录，递归处理
+        if [ -d "$item" ]; then
+            my_tree "$item" "${indent}│   "
+        fi
+    done
+}
+# 创建新项目模板文件
+function create_new_project_template_file {
+    local project_dir="$1/"
+    local project_name="$2"
+    local exe_name="$3"
 
+    local template_dir="$CONFIG_DIR/text_template/"
+
+    local path filename
+    for template_filename in $(ls "$template_dir"); do
+        
+        read -r path filename <<< "$(echo "$template_filename" | sed -n 's|\(.*\)_\(.*\)_template.txt|\1 \2|p')"
+        
+        if [ $path = "root" ]; then
+            path="$project_dir/"
+        else
+            path="$project_dir/$path/"
+        fi
+        
+        if [ -n "$filename" ]; then
+            mkdir -p "$path"
+            cat < "$template_dir/$template_filename" > "$path/$filename"
+            deal_macro "$path/$filename" "$PROJECT_NAME_MACRO" "$project_name"
+            deal_macro "$path/$filename" "$EXE_NAME_MACRO" "$exe_name"
+        fi
+    done
+
+    return 0
+}
+# 创建新模块模板文件
+function create_new_module_template_file {
+    local module_dir="$1/"
+    local module_name="$2"
+    local project_include_dir="$3/"
+
+    local module_include_dir="$module_dir/include/"
+    local template_dir="$CONFIG_DIR/text_template/"
+
+    mkdir -p "$module_dir/" "$project_include_dir" "$module_include_dir"
+    cat < "$template_dir/.module_CMakeLists.txt_template.txt" > "$module_dir/CMakeLists.txt"
+    cat < "$template_dir/.module_source_template.txt" > "$module_dir/$module_name.c"
+    cat < "$template_dir/.module_header_template.txt" > "$project_include_dir/$module_name.h"
+    # cat < "$template_dir/.module_private_header_template.txt" > "$module_include_dir/$module_name\_private.h"
+    deal_macro "$module_dir/CMakeLists.txt" "$MODULE_NAME_MACRO" "$module_name"
+    deal_macro "$module_dir/$module_name.c" "$MODULE_NAME_MACRO" "$module_name"
+    deal_macro "$project_include_dir/$module_name.h" "$MODULE_NAME_MACRO" "$module_name"
+    # deal_macro "$module_include_dir/$module_name\_private.h" "$module_name"
+
+    return 0
+}
+# 创建模块
+function create_module {
+    local project_dir="$1/"
+    local module_name="$2"
+    local src_dir="$project_dir/src/"
+    local module_dir="$src_dir/$module_name/"
+    local project_include_dir="$project_dir/include/"
+
+    if [ ! -d "$project_dir" ]; then
+        error "指定的项目目录不存在: $(make_path_pretty $project_dir) , 是否创建了该项目?"
+    fi
+    
+    local possible_conflict_file=$(echo "$module_dir" | sed -n 's|\(.*[^/]\)/*$|\1|p')
+    if [ -f "$possible_conflict_file" ]; then
+        remove_conflict "$possible_conflict_file" "存在同名文件: $(make_path_pretty $possible_conflict_file) , 无法创建项目!!!"
+    elif [ -d "$module_dir" ] && [ -n "$(ls -A $module_dir)" ]; then
+        remove_conflict "$module_dir" "目标目录: $(make_path_pretty $module_dir) 存在且非空, 可能存在冲突文件!!!"
+    fi
+
+    if [ $? -ne 0 ]; then
+        error "发生了冲突，且没有正确地删除冲突文件, 项目创建已取消!!!"
+    fi
+
+    mkdir -p "$module_dir/"
+    create_new_module_template_file "$module_dir" "$module_name" "$project_include_dir"
+
+    # 会破坏格式，故不使用(因为没有使用，所以这两句命令也没测试，慎用)
+    # sed -i "1i\add_subdirectory($module_name)" "$src_dir/CMakeLists.txt"
+    # sed -i "/target_link_libraries/{n ; a\\$module_name}" "$src_dir/CMakeLists.txt"
+
+    return 0
+}
 # 创建项目
 function create_project {
     local project_dir="$1/"
@@ -97,6 +212,7 @@ function create_project {
     local bin_dir="$project_dir/bin"
     local include_dir="$project_dir/include/"
     local build_dir="$project_dir/build/"
+    local lib_dir="$project_dir/lib/"
 
     local main_file="$src_dir/main.c"
     local cmakelists_file="$project_dir/CMakeLists.txt"
@@ -116,12 +232,8 @@ function create_project {
     fi
 
     echo "正在创建一个新的C项目, 位置为目录: $(make_path_pretty $project_dir)"
-    mkdir -p "$src_dir" "$bin_dir" "$include_dir" "$build_dir"
-    cat < "$CONFIG_DIR/main_template.txt" > "$main_file"
-    deal_macro "$main_file" "$PROJECT_NAME_MACRO" "$project_name"
-    cat < "$CONFIG_DIR/cmakelists_template.txt" > "$cmakelists_file"
-    deal_macro "$cmakelists_file" "$PROJECT_NAME_MACRO" "$project_name"
-    deal_macro "$cmakelists_file" "$EXE_NAME_MACRO" "$exe_name"
+    create_new_project_template_file "$project_dir" "$project_name" "$exe_name"
+    mkdir -p "$src_dir" "$bin_dir" "$include_dir" "$build_dir" "$lib_dir"
 
     if [ $create_git_flag = true ]; then
         echo "正在初始化git仓库(会自动将主分支命名为main并进行初始化提交)..."
@@ -133,22 +245,21 @@ function create_project {
         git add .
         git commit -m "Initial commit"
         cd - > /dev/null
-        echo "git仓库初始化完成."
+        echo "git仓库(包含忽略文件)初始化完成."
     fi
-
-    echo "成功创建以下目录或者文件:"
-    echo "directory: $(make_path_pretty $project_dir)"
-    echo "directory: $(make_path_pretty $src_dir)"
-    echo "directory: $(make_path_pretty $bin_dir)"
-    echo "directory: $(make_path_pretty $include_dir)"
-    echo "directory: $(make_path_pretty $build_dir)"
-    if [ $create_git_flag = true ]; then
-        echo "directory: $(make_path_pretty $project_dir/.git)"
-        echo "file: $(make_path_pretty $project_dir/.gitignore)"
-    fi
-    echo "file: $(make_path_pretty $main_file)"
-    echo "file: $(make_path_pretty $cmakelists_file)"
     echo
+    echo "成功创建以下目录或者文件:"
+    echo "====================="
+    echo 
+    which tree > /dev/null
+    if [ $? -eq 0 ]; then
+        tree "$project_dir"
+    else
+        echo "$(make_path_pretty $project_dir)"
+        my_tree "$project_dir"
+    fi
+    echo
+    echo "====================="
 
     return 0
 }
@@ -212,7 +323,7 @@ function run_execute {
 }
 
 # 处理选项与命令行参数
-set -- $(getopt bcghnr "$@")
+set -- $(getopt bcghmnr "$@")
 while [ -n "$1" ]; do
     # echo $1
     case "$1" in
@@ -220,6 +331,7 @@ while [ -n "$1" ]; do
         -c) IS_CREATE_PROJECT=true;;
         -g) IS_CREATE_ADDITIONAL_GIT=true;;
         -h) help; exit 0;;
+        -m) IS_CREATE_MODULE=true;;
         -n) IS_NO_OUTPUT=true;;
         -r) IS_RUN_EXECUTE=true;;
         --) shift; break;;
@@ -232,7 +344,7 @@ done
 if [ -z "$1" ]; then
     help
     error "缺少必要的参数!!!"
-elif [ ! $IS_CREATE_PROJECT = true ] && [ ! $IS_BUILD_PROJECT = true ] && [ ! $IS_RUN_EXECUTE = true ]; then
+elif [ ! $IS_CREATE_PROJECT = true ] && [ ! $IS_BUILD_PROJECT = true ] && [ ! $IS_RUN_EXECUTE = true ] && [ ! $IS_CREATE_MODULE = true ]; then
     help
     error "未指定任何操作!!!"
 fi
@@ -241,6 +353,17 @@ if [ $IS_NO_OUTPUT = true ]; then
     exec 1>/dev/null
 fi
 
+# 创建一个模块
+if [ $IS_CREATE_MODULE = true ]; then
+    if [ ! $# -eq 2 ]; then
+        help
+        error "创建模块时必须且只能指定一个项目目录和一个模块名!!!"
+    fi
+    create_module "$1" "$2"
+    exit $?
+fi
+
+# 创建项目、编译、执行
 while [ -n "$1" ]; do
     agrument_target_dir="$1"
     result=0
